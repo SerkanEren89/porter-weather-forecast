4377cfb04cead5770fb73bf53b8dbafd73fd6aec
eren <Start#10>
2019-05-24 18:08:13 +0300

    initial commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,28 +1,31 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+  .controller('WeatherCtrl', function($scope, Weathers) {
+    Weathers.dailyWeather().then(function(result){
+      $scope.weathers=result.daily.data;
+      $scope.latitude = result.latitude;
+      $scope.longitude = result.longitude;
+      $scope.icon = result.daily.iconToShow;
+    });
+  })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+  .controller('WeatherDetailCtrl', function($scope, Weathers) {
+    Weathers.hourlyWeather().then(function(result){
+      $scope.hourlyWeathers=result.hourly.data;
+      $scope.dailyWeathers=result.daily.data;
+    });
+  })
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+  .controller('PlacesCtrl', function($scope, Places) {
+    $scope.places = Places.placesWeather();
+  })
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
-
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+  .controller('SettingsCtrl', function($cookies, $scope, Languages) {
+    $scope.settings = Languages.all().then(function(result){
+      $scope.settings = result;
+    });
+    
+    $scope.selectLanguage = function (setting) {
+      $cookies.setting = setting;
+    }
+  });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,125 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+  .factory('SettingFactory', function () {
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+    var setting = {
+      abbr: 'en',
+        language: 'English'
+    };
+    return {
+      getSetting: function () {
+        return setting;
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
+      },
+      setSetting: function (newSetting) {
+        setting = newSetting;
       }
-      return null;
-    }
-  };
-});
+    };
+  })
+
+  .factory('PlaceFactory', function () {
+    var place = {
+      id: 0,
+      name: 'Berlin',
+      latitude: '52.520008',
+      longitude: '13.404954'
+    };
+    return {
+      getPlace: function () {
+        return place;
+
+      },
+      setPlace: function (newPlace) {
+        place = newPlace;
+      }
+    };
+  })
+
+  .factory('Weathers', function ($http, SettingFactory, PlaceFactory) {
+    var place = PlaceFactory.getPlace();
+    var setting = SettingFactory.getSetting();
+    var baseUrl = "http://localhost:8080/weathers" ;
+    return {
+      dailyWeather: function () {
+        var dailyUrl = baseUrl + "/daily?latitude=" + place.latitude + "&longitude=" + place.longitude + "&lang=" + setting.abbr;
+        return $http.get(dailyUrl).then(function (response) {
+          return response.data;
+        });
+      },
+      hourlyWeather: function () {
+        var hourlyUrl = baseUrl + "/hourly?latitude=" + place.latitude + "&longitude=" + place.longitude + "&lang=" + setting.abbr;
+        return $http.get(hourlyUrl).then(function (response) {
+          return response.data;
+        });
+      }
+    };
+  })
+
+  .factory('Places', function ($http, SettingFactory) {
+    var places = [{
+      id: 0,
+      name: 'Berlin',
+      latitude: '52.520008',
+      longitude: '13.404954'
+    }, {
+      id: 1,
+      name: 'Paris',
+      latitude: '48.856613',
+      longitude: '2.352222'
+    }, {
+      id: 2,
+      name: 'Amsterdam',
+      latitude: '52.370216',
+      longitude: '4.895168'
+    }, {
+      id: 3,
+      name: 'Barcelona',
+      latitude: '41.385063',
+      longitude: '2.173404'
+    }];
+
+    return {
+      all: function () {
+        return places;
+      },
+      placesWeather: function () {
+        var baseUrl = "http://localhost:8080/weathers";
+        var placesData = [];
+
+        for (var i = 0; i < places.length; i++) {
+          var place = places[i];
+          (function (places) {
+            var setting = SettingFactory.getSetting();
+            var dailyUrl = baseUrl + "/daily?latitude=" + place.latitude + "&longitude=" + place.longitude + "&lang=" + setting.abbr;
+            $http.get(dailyUrl).then(function (response) {
+              var pushedObject = {
+                data: response.data.daily.data[0],
+                place: places,
+                icon: response.data.daily.iconToShow
+              };
+              placesData.push(pushedObject);
+            });
+          }(place));
+        }
+        return placesData;
+      },
+      dailyWeather: function () {
+        var dailyUrl = baseUrl + "/daily";
+        var setting = SettingFactory.getSetting();
+        return $http.get(dailyUrl).then(function (response) {
+          return response.data.daily.data;
+        });
+      },
+    };
+  })
+
+  .factory('Languages', function ($http) {
+    var baseUrl = "http://localhost:8080/languages";
+    return {
+      all: function () {
+        return $http.get(baseUrl).then(function (response) {
+          return response.data;
+        });
+      }
+    };
+  });
